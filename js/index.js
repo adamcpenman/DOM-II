af351d1246a54541a23898f0a8c96d8b70e5abd2
Your Name <you@example.com>
2019-08-10 12:42:00 -0500

    Added another Event Listener and added the preventDefault. I have a alot of different things comment out, I have just been playing around with different ways to do things

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
-const navBar = document.querySelector('.nav-link');
+
+//Nav Bar
+const navBar = Array.from(document.getElementsByClassName('nav-link'));
 
 // navBar[1].addEventListener('click', cb => {
 //     console.log("clicked");
@@ -7,40 +9,66 @@ const navBar = document.querySelector('.nav-link');
 
 console.log(navBar);
 
-navBar.addEventListener("mouseenter", event =>{
-event.target.style.color = "purple";
+//This times out the color
 
-setTimeout( () => {
-    event.target.style.color="";
-}, 500);
-}, false);
+// navBar.addEventListener("mouseenter", event =>{
+// event.target.style.color = "purple";
+
+// setTimeout( () => {
+//     event.target.style.color="";
+// }, 500);
+// }, false);
+
+//Mouse enters, link change, mouse leaves, like goes back
+
+navBar.forEach(link => {
+  link.addEventListener("mouseenter", ev => {
+    link.style.color = "yellow";
+    ev.preventDefault();
+
+    link.addEventListener("mouseleave", ev => {
+    link.style.color = "";
+    });
+  });
+});
+
+//Img head diseappers then reappears 
 
 const img = document.querySelector('.intro img');
 console.log(img);
 img.addEventListener('dblclick', (event) => {
-    img.style.height = '50px';
-    img.style.width = '50px';
+    img.style.display = 'none';
+    // img.style.width = '50px';
 
     setTimeout( () => {
-        event.target.style.height = "";
-        event.target.style.width = "";
-    }, 500);
+        img.style.display = "";
+    }, 1000);
 });
 
 const midImg = document.querySelector('.img-content');
 console.log(midImg);
 
 midImg.addEventListener('mouseover', () => {
-    midImg.style.height = "50px";
-    midImg.style.width = "50px";
+    midImg.style.height = "200px";
+    midImg.style.width = "200px";
+
+    setTimeout( () => {
+        midImg.style.height = "";
+        midImg.style.width = "";
+    }, 500);
+
 });
 
 const headFun = document.querySelector('.logo-heading');
 console.log(headFun);
 
-headFun.addEventListener('load', () => {
+// headFun.addEventListener('load', () => {
     
-})
+// })
+
+// window.addEventListener('load', (event) => {
+//     alert('page is loaded')
+// })
 
 const adventureAwaits = document.querySelector('.inverse-content .text-content');
 console.log(adventureAwaits);
@@ -63,7 +91,42 @@ intro.addEventListener('keyup', function () {
 }, true);
 
 
-window.addEventListener('load', (event) => {
-    alert('page is loaded')
+// const funBus = document.getElementsByClassName('logo-heading');
+// console.log(funBus, "logo");
+// funBus[0].addEventListener('keydown', function () {
+// funBus.style.color = "red";
+// })
+
+const allParagraphs = document.querySelectorAll('p');
+console.log(allParagraphs)
+
+window.addEventListener('keydown', function(e) {
+  let keyDown = e.key;
+  if(keyDown === 'Enter') {
+    allParagraphs.forEach(p => p.classList.toggle('bigP'));
+    console.log("Big paragraphs");
+  } 
+  
+});
+    
+
+const button = document.querySelector('.btn');
+console.log(button);
+
+button.addEventListener('click', (event) => {
+event.target.style.color = "red";
 })
+
+const adventureImg = document.querySelector('.img-fluid');
+console.log(adventureImg);
+adventureImg.addEventListener('mouseover', () => {
+    adventureImg.style.width = '30rem';
+    adventureImg.style.height = '30rem';
+    // adventureImg.setAttribute('src', 'img/fun.jpg');
+
+adventureImg.addEventListener('mouseleave', () => {
+    adventureImg.style.width = "";
+    adventureImg.style.height = "";
+})
+}) 
 
